966fff5e7c8381fc6e04541fcdb4796bdbc6a0b3
kristofeka <kristofeka123@gmail.com>
2018-10-26 00:06:59 +0700

    delete eng.php

--- eng.php
+++
@@ -1,9 +0,0 @@
-<?php
-$lang_beranda = "home";
-$lang_produk = "Product";
-$lang_company = "Company";
-$lang_tentang_kami = "About Us";
-$lang_karir = "Carrer";
-$lang_dokument = "Document";
-$lang_hubungi = "Contact Us";
-?>
