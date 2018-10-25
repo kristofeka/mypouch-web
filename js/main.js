a550796ce95aeeb5603541a752395e23ed275ad1
kristofeka <kristofeka123@gmail.com>
2018-10-26 00:56:35 +0700

    new function for button language

--- js/main.js
+++ js/main.js
@@ -145,6 +145,23 @@
 
 	var dropdown = function() {
 
+		$('.has-dropdown1').mouseenter(function(){
+
+			var $this = $(this);
+			$this
+				.find('.dropdown')
+				.css('display', 'block')
+				.addClass('animated-fast fadeInUpMenu');
+
+		}).mouseleave(function(){
+			var $this = $(this);
+
+			$this
+				.find('.dropdown')
+				.css('display', 'none')
+				.removeClass('animated-fast fadeInUpMenu');
+		});
+
 		$('.has-dropdown').mouseenter(function(){
 
 			var $this = $(this);
