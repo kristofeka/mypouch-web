a8c189b7d9b30d4f6eb4e72e8a460c37de6f9f38
kristofeka <kristofeka123@gmail.com>
2018-10-27 20:29:47 +0700

    new modif page for syarat ketentuan

---
+++ SK.php
@@ -0,0 +1,201 @@
+<?php include 'header.php' ?>
+
+	<header id="fh5co-header" class="fh5co-cover fh5co-cover-sm" role="banner" style="background-image:url(images/head_bg3.png); height: 100%; " data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center">
+					<div class="display-t">
+						<div class="display-tc animate-box" data-animate-effect="fadeIn">
+							<h1 style="color:#fff"><strong>Syarat & Ketentuan</strong></h1>
+							
+							
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
+
+	<div id="fh5co-services" style="padding:3em 0 !important; background-image:url(images/head_bg4.png); background-repeat: no-repeat;  background-size: 100% 100%;" >
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-8">
+					<p class="isiSK">Dengan menggunakan Layanan ini, Pengguna dianggap telah menyetujui dan tunduk terhadap ketentuan penggunaan layanan MyPouch. Oleh karena itu Pengguna wajib membaca, memahami dan meneliti seluruh syarat dan ketentuan yang sudah dibuat oleh MyPouch. Jika Pengguna tidak menyetujui syarat dan ketentuan yang telah dibuat oleh MyPouch maka Pengguna dapat berhenti untuk menggunakan layanan ini.</p>
+					<ol class="olcus">
+						<li id="a" class="licus">Layanan MyPouch</li>
+							<p class="pcus">
+							MyPouch merupakan sebuah produk yang dikeluarkan oleh PT. Dananjaya Digital Indonesia yang memberikan layanan Pengiriman Dana (disbursement). Pengguna dapat menyesuaikan kebutuhan pengiriman dana dengan layanan yang diberikan oleh MyPouch :
+							</p>
+							<ol>
+							<li class="licus2">Direct Disbursement 
+								<p class="pcus2">
+								Direct Disbursement merupakan sebuah layanan layanan transfer dana yang memberikan perintah transfer real time dari Pengguna ke rekening tujuan melalui system API MyPouch. 
+								</p>
+							</li>
+							<li class="licus2">Batch Disbursement
+								<p class="pcus2">
+								Batch Disbursement merupakan sebuah layanan transfer dana yang dapat melakukan pengiriman dana dalam jumlah banyak secara sekaligus dengan cara mengunggah file excel. Ketika transaksi telah berhasil dilakukan, maka Pengguna akan menerima e-mail yang berisikan notifikasi dan penerima dana akan menerima bukti transfer. Untuk layanan Batch Disbursement, tidak ada limitasi harian jumlah transaksi maupun jumlah batch yang dapat diproses. Akan tetapi MyPouch memiliki batas 1000 transaksi setiap batch nya. Misalnya, ketika Pengguna memiliki kebutuhan untuk melakukan disbursement sebanyak 10.000 transaksi, maka Pengguna harus mengunggah 10 file excel Batch Disbursement, yang masing-masing berisikan 1000 transaksi, ke dasbor.
+								</p>
+							</li>
+							</ol>
+						<li id="b" class="licus">Ketentuan Pengguna Layanan MyPouch</li>
+							<p class="pcus">
+							Untuk keperluan pelaksanaan Perjanjian ini, Pengguna wajib melakukan registrasi account dan mengisi sejumlah saldo yang digunakan sebagai Rekening Operasional dan/atau untuk keperluan Operasional lainnya. 
+							</p>
+							<ol>
+								<li class="pcus3">
+									Wajib menyatakan sebagai suatu Perseroan Terbatas/Perseorangan yang cakap hukum dan dapat mempertangungjawabkan segala perbuatan dan tindakannya secara hukum.
+								</li>
+								<li class="pcus3">
+									Bagi Perusahaan wajib memiliki Legalitas yang sah dalam menjalankan kegiatan usahanya dan dapat mempertanggungjawabkan segala tindakan yang dianggap sebagai suatu kelalaian/pelanggaran atas syarat dan ketentuan pengguna layanan MyPouch.
+								</li>
+								<li class="pcus3">
+									Wajib memberikan data dan informasi dengan baik dan benar, tidak menyesatkan, dan/atau bersifat pemalsuan data.
+								</li>
+								<li class="pcus3">
+									Registrasi account dan pengisian saldo rekening wajib untuk tunduk pada ketentuan MyPouch
+								</li>
+								<li class="pcus3">
+									Dalam keadaan apapun bagi pengguna layanan MyPouch yang sudah mengisi saldonya di MyPouch maka saldo tersebut tidak dapat di tarik kembali dan/atau tidak dapat di uangkan. 
+								</li>
+								<li class="pcus3">
+									Dalam hal terjadi perubahan syarat dan ketentuan Pengguna yang berlaku di MyPouch, maka Pihak MyPouch akan melakukan pemberitahuan secara tertulis kepada Pengguna selambat-lambatnya 7 (Tujuh) Hari Kerja setelah perubahan ketentuan tersebut diberlakukan secara umum.
+								</li>
+							</ol>
+						<li id="c" class="licus">Kebijakan Transfer Dana </li>
+							<ol>
+								<li class="pcus3">
+									Jumlah nominal yang akan di transfer sesuai perintah nominal transfer yang telah tertera pada sistem.
+								</li>
+								<li class="pcus3">
+									MyPouch akan mengirimkan dana kerekening tujuan sesuai dengan nama penerima rekening yang diperintahkan oleh pengguna melalui sistem
+								</li>
+								<li class="pcus3">
+									MyPouch tidak akan memproses pengiriman apabila nama yang tertera pada penerima dana berbeda pada data yang di berikan pada MyPoch.
+								</li>
+								<li class="pcus3">
+									MyPouch tidak akan memproses pengiriman apabila jumlah transfer tidak sesuai dengan total saldo yang dimiliki.
+								</li>
+								<li class="pcus3">
+									Untuk pengiriman yang di batalkan karna ada ketidaksesuaian pada data maka Refund akan membutuhkan waktu (-----).
+								</li>
+								<li class="pcus3">
+									MyPouch berhak menangguhkan transaksi jika dalam perintah transaksi kami mengetahui bahwa ada unsur tindak pidana pencucian uang.
+								</li>
+								<li class="pcus3">
+									Dana yang telah di TopUp ke rekening/sistem MyPouch tidak di tarik kembali dalam bentuk apapun.
+								</li>
+								<li class="pcus3">
+									Jika di suatu hari pengguna layanan MyPouch mengalami kendala dalam pelaksanaan operasionalnya yang mengakibatkan suatu kerugian serta berhentinya kerja sama maka dalam hal ini MyPouch tidak dapat mengembalikan dana yang telah di TopUp pada sistem dan jika berhentinya kerja sama ini masih dalam ikatan kontrak kerja sama maka si Pengguna wajib membayarkan Penalty yang ketentuannya telah di atur dalam kontrak perjanjian kerja sama yang telah disepakati sebelumnya.
+								</li>
+								<li class="pcus3">
+									Pihak MyPouch berhak menghentikan kerjasama dan/atau menonaktifkan account Pengguna baik sementara ataupun seterusnya jika terdapat pelanggaran syarat dan ketentuan yang telah MyPouch tentukan. 
+								</li>
+							</ol>
+						<li id="d" class="licus">Biaya Layanan</li>
+							<ol>
+								<li class="pcus3">
+									MyPouch dengan ini akan memberikan tarif kepada Pengguna layanan atas biaya-biaya yang dikeluarkan berkaitan dengan pelaksanaan Perjanjian ini. Penentuan tariff tersebut akan diatur lebih lanjut saat pertemuan antara pihak MyPouch dengan Pengguna layanan.
+								</li>
+								<li class="pcus3">
+									Hasil kesepakatan yang diperoleh atas penentuan biaya layanan akan dituangkan kedalam perjanjian kerjasama.
+								</li>
+								<li class="pcus3">
+									Perubahan atas biaya-biaya sebagaimana dimaksud pada angka 1 dan 2 dilakukan berdasarkan kesepakatan Para Pihak, dan kesepakatan tersebut cukup dilakukan dengan surat menyurat yang merupakan lampiran dan bagian yang tidak terpisahkan dari Perjanjian ini.
+								</li>
+								<li class="pcus3">
+									Dalam hal terdapat pengembalian dana yang dilakukan oleh MyPouch kepada Pengguna dikarenakan adanya ketidaksesuaian dan/atau sebab apapun lainnya, pengembalian dana tersebut merupakan sejumlah dana setelah dikurangi dengan biaya transfer antar bank yang besarnya sebagaimana ditetapkan dalam Perjanjian kerjasama.
+								</li>
+								<li class="pcus3">
+									Pembebanan biaya-biaya tersebut dapat di-review secara periodik setiap tahun oleh Pengguna Layanan.
+								</li>
+							</ol>
+						<li id="e" class="licus">Contingency Plan</li>
+							<p class="pcus">
+								Dalam hal terjadi gangguan terhadap sistem fasilitas MyPouch, termasuk namun tidak terbatas pada kegagalan sistem komunikasi/transmisi dan atau kegagalan peralatan elektronik lainnya, yang menyebabkan sistem tersebut mati atau tidak berfungsi semestinya, maka PARA PIHAK sepakat untuk menggunakan sistem perbankan secara manual/konvensional, dengan mengesampingkan beberapa ketentuan dalam Perjanjian ini yang terkait untuk sementara waktu, sampai dengan gangguan tersebut dapat diatasi.
+							</p>
+						<li id="f" class="licus">Kekayaan Intelektual </li>
+							<ol>
+								<li class="pcus3">
+									Pengguna layanan MyPouch dilarang untuk menggunakan logo, dan/atau merek dagang MyPouch tanpa persetujuan dari pihak MyPouch
+								</li>
+								<li class="pcus3">
+									Seluruh isi konten pada website MyPouch tidak terbatas pada teks dan grafik merupakan milik MyPouch sepenuhnya.
+								</li>
+								<li class="pcus3">
+									Berdasarkan syarat dan ketentuan ini maka Pengguna layanan tidak mendapatkan hak kekayaan intelektual apapun atas penggunaan logo dan/atau merk dagang MyPouch
+								</li>
+								<li class="pcus3">
+									Pelanggaran atas ketentuan ini merupakan pelanggaran atas perjanjian dan pihak MyPouch berhak untuk mengajukan tuntutan hukum dan ganti rugi untuk kerugian yang ditimbulkan.
+								</li>
+							</ol>
+						<li id="g" class="licus">Batasan dan Tanggung Jawab</li>
+							<ol>
+								<li class="pcus3">
+									MyPouch tidak bertanggung jawab atas suatu kelalaian atau kesalahan pengguna dalam memberikan informasi nomor rekening tujuan yang tidak sesuai yang nantinya akan berimbas pada suatu kerugian bagi pengguna ataupun nasabah dari pengguna layanan.
+								</li>
+								<li class="pcus3">
+									MyPouch hanya memiliki hubungan kerja sama dengan Pengguna layanan, tidak dengan Penerima Dana. Segala sesuatu yang terjadi diantara keduanya maka hal tersebut di luar tanggung jawab MyPouch.
+								</li>
+							</ol>
+						<li id="h" class="licus">Ketersediaan dan Penggunaan </li>
+							<p class="pcus">
+								Pengguna harus memastikan bahwa akses dan penggunaan layanan ini adalah sah secara hukum atau tidak dilarang oleh peraturan dan perundang-undangan yang berlaku.
+							</p>
+						<li id="i" class="licus">Kebijakan Privasi </li>
+							<p class="pcus">
+							Segala ketentuan yang di sebutkan pada halaman ini mengatur hal-hal yang menyangkut data para penerima dana yang di berikan oleh Pengguna Layanan MyPouch dalam rangka pemanfaatan fasilitas, fitur jasa dan/atau layanan yang ditawarkan oleh MyPouch. Ketentuan-ketentuan menyangkut data mengikat seluruh pengguna layanan tanpa terkecuali untuk tunduk dan patuh atas ketentuan yang telah di tetapkan Mypouch.
+							<br>Berikut ini ketentuan-ketentuan yang menyangkut data tersebut :
+							</p>
+							<ol>
+								<li class="pcus3">
+									Selama berlakunya Perjanjian ini, Para Pihak wajib menyimpan dan menjaga kerahasiaan seluruh informasi dan data yang bersifat rahasia dan tidak diperkenankan/mengijinkan pihak ketiga bertindak dengan cara apapun untuk mempublikasikan, menyebarkan, menawarkan, menyerahkan atau mengalihkan informasi dan data dimaksud kepada pihak manapun, kecuali ada persetujuan tertulis dari Pihak pemilik informasi dan data atau dalam rangka memenuhi ketentuan perundang-undangan yang berlaku.
+								</li>
+								<li class="pcus3">
+									Para Pihak sepakat dan setuju bahwa segala informasi dan data baik yang tertulis maupun yang direkam dalam penyimpanan memori yang dimiliki oleh Para Pihak di dalam sistem secara keseluruhan yang terdapat dalam dokumentasi program dan informasi-informasi lain yang berkaitan dengan bisnis, produk dan pelayanan yang diketahui atau timbul berdasarkan Perjanjian ini adalah bersifat rahasia.
+								</li>
+								<li class="pcus3">
+									Apabila salah satu Pihak melanggar ketentuan sebagaimana dimaksud pada angka (1) dan (2) ketentuan ini, maka segala kerugian, tuntutan/gugatan dari pihak ketiga manapun juga yang mungkin  timbul  dan  dialami  oleh  Pihak  yang  dilanggar,  merupakan  tanggung  jawab sepenuhnya dari Pihak  yang melanggar.
+								</li>
+								<li class="pcus3">
+									Sehubungan dengan hal tersebut, maka Pihak yang melanggar berkewajiban untuk memberikan ganti kerugian kepada pihak yang dirugikan.
+								</li>
+							</ol>
+						<li id="j" class="licus">Penyelesaian Perselisihan </li>
+							<ol>
+								<li class="pcus3">
+									Apabila timbul perselisihan mengenai penafsiran dan/atau pelaksanaan Perjanjian ini, Para Pihak sepakat untuk menyelesaikan secara musyawarah untuk mufakat.
+								</li>
+								<li class="pcus3">
+									Apabila perbedaan pendapat/perselisihan tersebut di atas tidak dapat diselesaikan secara musyawarah, maka Para Pihak sepakat untuk menyelesaikan perbedaan pendapat/perselisihan tersebut melalui Pengadilan.
+								</li>
+								<li class="pcus3">
+									Para Pihak sepakat memilih tempat kedudukan (domisili) hukum yang tetap dan seumumnya di Kantor Kepaniteraan Pengadilan Negeri Jakarta Pusat di Jakarta.
+								</li>
+							</ol>
+					</ol>
+					
+
+				</div>
+				<div class="hidden-xs col-md-4">
+					<ol class="olcus4 ">
+						<li class="licus4"><a href="#a">Layanan MyPouch</a></li>
+						<li class="licus4"><a href="#b">Ketentuan Pengguna Layanan MyPouch</a></li>
+						<li class="licus4"><a href="#c">Kebijakan Transfer Dana</a></li>
+						<li class="licus4"><a href="#d">Biaya Layanan</a></li>
+						<li class="licus4"><a href="#e">Contingency Plan</a></li>
+						<li class="licus4"><a href="#f">Kekayaan Intelektual</a></li>
+						<li class="licus4"><a href="#g">Batasan dan Tanggung Jawab</a></li>
+						<li class="licus4"><a href="#h">Ketersediaan dan Penggunaan</a></li>
+						<li class="licus4"><a href="#i">Kebijakan Privasi</a></li>
+						<li class="licus4"><a href="#j">Penyelesaian Perselisihan</a></li>
+					</ol>
+				</div>
+			</div>
+		</div>
+	</div>
+
+<?php include 'foot.php' ?>
+
